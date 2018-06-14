3c7939e5c6a30a2418d1bf84452a115a89b7a2db
damienbod <damien_bod@hotmail.com>
2018-06-14 22:28:45 +0200

    Adding some forms

---
+++ MvcDynamicDropdownList/Controllers/BackController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MvcDynamicDropdownList.Models;
+using System.Collections.Generic;
+
+namespace MvcDynamicDropdownList.Controllers
+{
+    public class BackController : Controller
+    {
+        public IActionResult IndexPost()
+        {
+            return View();
+        }
+
+        public IActionResult IndexGet()
+        {
+            return View();
+        }
+
+        // SomeDataFromAPost
+        [HttpPost]
+        public IActionResult SomeDataFromAPost(string dummyData)
+        {
+            var model = new DdlItems
+            {
+                Items = new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "H1", Value = "H1Value"},
+                    new SelectListItem { Text = "This is cool", Value = "cool"}
+                }
+            };
+            return View("IndexPost", model);
+        }
+
+        // SomeDataFromAPostGet
+        [HttpGet]
+        public IActionResult SomeDataFromAGet(string dummyData)
+        {
+            var model = new DdlItems
+            {
+                Items = new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "H1", Value = "H1Value"},
+                    new SelectListItem { Text = "This is cool", Value = "cool"}
+                }
+            };
+            return View("IndexGet", model);
+        }
+    }
+}
